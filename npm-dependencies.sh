4a90382edaa8f0f2104a01e695015a2a1c261c9f
Guilherme Vieira <guivpw68@gmail.com>
2022-04-04 17:22:37 -0300

    install npm packages

---
+++ npm-dependencies.sh
@@ -0,0 +1,13 @@
+yarn global add expo-cli \
+	autocannon \
+	create-react-app \
+	react-native-cli \
+	npm-check \
+	expo-cli \
+	@nestjs/cli \
+	ntl \
+	@expo/ngrok \
+	create-next-app \
+	netlify-cli \
+	sb \
+	serve
